ab72a5f14dc63b9870b49e259dea8afad10a2964
Anton Vykhlo <a.vykhlo@levi9.com>
2019-08-27 15:20:44 +0300

    Add project files.

---
+++ FluentBuilder/Item.cs
@@ -0,0 +1,33 @@
+﻿namespace FluentBuilder
+{
+    public class Item
+    {
+        public string Name;
+        public int Quantity;
+    }
+
+    public class ItemNameBuilder
+    {
+        protected Item _item = new Item();
+
+        public ItemNameBuilder WithName(string name)
+        {
+            _item.Name = name;
+            return this;
+        }
+
+        public Item Build()
+        {
+            return _item;
+        }
+    }
+    public class ItemQuantityBuilder : ItemNameBuilder
+    {
+
+        public ItemQuantityBuilder WithQuantiry(int quantity)
+        {
+            _item.Quantity = quantity;
+            return this;
+        }
+    }
+}
---
+++ FluentBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = new ItemQuantityBuilder().WithName("asd").Build();
+        }
+    }
+}
